201a749135b60714a79d91e3dce275819b180e8b
Obasi Kong <apexbugfinder@gmail.com>
2022-12-14 00:53:54 -0800

    added methods to classes

--- flask/sbj/src/models/player.py
+++ flask/sbj/src/models/player.py
@@ -1,5 +1,5 @@
 import datetime
-from . import db, Hand
+from . import db, Hand, HandStatus
 
 class Player(db.Model):
           __tablename__='players'
@@ -19,3 +19,17 @@ class Player(db.Model):
                     'name': self.name,
                     'limit': self.limit
                   }
+
+
+          def change_limit(self, new_limit):
+                    # print(f"{self.name} your limit {self.limit} is being changed to {new_limit}")
+                    self.limit = new_limit
+                    self.hand.player_limit = new_limit
+                    # print(f"{self.name} your new limit is {self.limit}")
+                    return self.limit
+
+          def add_to_hand(self, cards ):
+                if self.hand.status == HandStatus.ACTIVE.name:
+
+
+                      self.hand.add_to_hand(cards)
